5ee1299a41b8b16935bafc5c4ec02f7a2f054818
Alexandre KOVAC <contact@kovacou.com>
2021-02-27 16:59:41 +0100

    add godoc

--- bool.go
+++ bool.go
@@ -1,3 +1,6 @@
+// Copyright © 2021 Alexandre Kovac <contact@kovacou.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 package convert
-
-
